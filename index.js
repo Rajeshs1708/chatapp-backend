3285ed9209d7e7f7679f2c1eec9285d50f017447
Rajesh <rajeshs1097@gmail.com>
2023-03-14 01:08:32 +0530

    first commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const server = require('http').createServer(app)
 const PORT = 5001
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://chatapp-03-23.netlify.app',
+    origin: '*',
     methods: ['GET', 'POST']
   }
 })
